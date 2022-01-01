e249ce6b3edf35ca7a6c9743c99ec9d0f2ec6a07
nebula1989 <benwalterscoding@gmail.com>
2022-01-01 15:57:54 -0800

    added generic index and detail views

--- order_parts/urls.py
+++ order_parts/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from . import views
-from order_parts.views import PartsOrdersListView, PartsOrdersArchiveView
+from order_parts.views import IndexView, PartsOrdersArchiveView
 
 urlpatterns = [
-    path('', views.part_order_form, name='part_order_form'),
-    path('AllPartOrders/', PartsOrdersListView.as_view(), name='all_part_orders'),
+    path('', IndexView.as_view(), name='PartsOrderedIndex'),
+    path('partsForm', views.part_order_form, name='part_order_form'),
+    path('detail/<int:part_order_id>/', views.detail, name='detail'),
     path('today/', PartsOrdersArchiveView.as_view(), name='todays_part_orders'),
 ]
--- order_parts/views.py
+++ order_parts/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
+from django.http import Http404
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
 from django.views.generic.dates import TodayArchiveView
 from .forms import PartOrderingForm
@@ -20,13 +25,30 @@ def part_order_form(request):
     return render(request, 'order_parts/order_parts.html', context)
 
 
-class PartsOrdersListView(ListView):
+class IndexView(ListView):
     model = OrderPart
-    template_name = 'order_parts/view_part_orders.html'
+    context_object_name = 'latest_partsordered_list'
+    template_name = 'order_parts/index.html'
+
+    def get_queryset(self):
+        """Return the last ten ordered parts."""
+        return OrderPart.objects.filter(
+            date_ordered__lte=timezone.now()
+        ).order_by('date_ordered')[:10]
+
+
+def detail(request, part_order_id):
+    try:
+        part_order = OrderPart.objects.get(pk=part_order_id)
+    except OrderPart.DoesNotExist:
+        raise Http404("Part Order does not exist")
+    return render(request, 'order_parts/detail.html',
+                  {'part_order': part_order})
 
 
 class PartsOrdersArchiveView(TodayArchiveView):
     queryset = OrderPart.objects.all()
+    context_object_name = 'todays_partsordered_list'
     date_field = "date_ordered"
-    template_name = 'order_parts/view_part_orders.html'
+    template_name = 'order_parts/index.html'
 
