cc3fc32145caf58fdb80549ccc44290b86605256
nebula1989 <benwalterscoding@gmail.com>
2022-01-07 15:40:44 -0800

    created simple landingpage

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from order_parts import views as order_part_views
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('', order_part_views.IndexView.as_view(), name='PartsOrderedIndex'),
 ]
