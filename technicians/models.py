53f439dbae01113284a648628a31c9d2220169b2
nebula1989 <benwalterscoding@gmail.com>
2022-01-07 20:30:07 -0800

    added technician model data and to admin

--- technicians/models.py
+++ technicians/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+class Technician(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    location = models.CharField(max_length=30)
+    phone_number = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "%s %s" % (self.first_name, self.last_name)
