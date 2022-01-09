f43350a101452a7a744aeda3e307f27fc5705b7f
nebula1989 <benwalterscoding@gmail.com>
2022-01-09 10:13:13 -0800

    added ordered by tech column in partsordered model

--- order_parts/models.py
+++ order_parts/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from technicians.models import Technician
 
 
 # Create your models here.
@@ -8,6 +9,7 @@ class PartOrder(models.Model):
     part_type = models.CharField(max_length=40, default='none')
     reason_needed = models.TextField(max_length=250, default='none')
     date_ordered = models.DateTimeField(auto_now=True)
+    ordered_by = models.ForeignKey(Technician, on_delete=models.CASCADE, default=None)
 
     def __str__(self):
         return self.id
