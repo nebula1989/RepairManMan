9c0d3480963421c8549ecf40b017ec338a16fcf4
nebula1989 <benwalterscoding@gmail.com>
2022-01-09 09:01:58 -0800

    created job models

--- jobs/models.py
+++ jobs/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from technicians.models import Technician
+
 
 # Create your models here.
+class Job(models.Model):
+    status_choices = (
+        ('FV', 'First Visit / Troubleshoot'),
+        ('AWP', 'Awaiting Part'),
+        ('INP', 'In Progress'),
+        ('C', 'Completed')
+    )
+    status = models.CharField(choices=status_choices, max_length=50)
+    service_notes = models.TextField(max_length=250, default="No Notes Provided")
+    assigned_technician = models.ForeignKey(Technician, models.DO_NOTHING)
+
+    def __str__(self):
+        return "%d: %s" % (self.pk, self.status)
