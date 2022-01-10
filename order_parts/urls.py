7ecde58feb4bb5dd6b13b33683c418d9caa12634
nebula1989 <benwalterscoding@gmail.com>
2022-01-09 19:16:37 -0800

    fixed errors with part ordering

--- order_parts/urls.py
+++ order_parts/urls.py
@@ -7,8 +7,8 @@ from order_parts.views import IndexView, PartsOrdersArchiveView, PartsOrdersUpda
 urlpatterns = [
     path('', IndexView.as_view(), name='PartsOrderedIndex'),
     path('partsForm', views.part_order_form, name='part_order_form'),
-    path('detail/<int:part_order_id>/', views.detail, name='detail'),
-    path('<pk>/update', PartsOrdersUpdateView.as_view(), name='update'),
+    path('detail/<int:part_order_id>/', views.detail, name='detail_part'),
+    path('<pk>/update', PartsOrdersUpdateView.as_view(), name='update_part'),
     path('today/', PartsOrdersArchiveView.as_view(), name='todays_part_orders'),
-    path('<pk>/delete/', PartsOrderDeleteView.as_view(), name='delete'),
+    path('<pk>/delete/', PartsOrderDeleteView.as_view(), name='delete_part'),
 ]
